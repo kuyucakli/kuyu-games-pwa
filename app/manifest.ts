154763ee0f1443ac11e843ba7c653b3df4938469
Burak Kuyucaklı <burak.kuyucakli@gmail.com>
2025-10-10 21:01:51 +0300

    Start...pwa

---
+++ app/manifest.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Next.js PWA",
+    short_name: "NextPWA",
+    description: "A Progressive Web App built with Next.js",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/icon-180.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
